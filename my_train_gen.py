60a5829021eeb285d4a708ab3c7e92c6d1cfa9ab
Github-Tree-0 <1900012967@pku.edu.cn>
2022-10-26 19:35:49 +0800

    a

--- my_train_gen.py
+++ my_train_gen.py
@@ -79,7 +79,7 @@ def train(args, model, optimizer, scheduler, it):
     model.train()
 
     # Forward
-    loss, neg_elbo, std = model.get_loss(min(it/300000) * args.max_std_weight)
+    loss, neg_elbo, std = model.get_loss(min(it/300000, 1.0) * args.max_std_weight)
 
     # Backward and optimize
     loss.backward()
